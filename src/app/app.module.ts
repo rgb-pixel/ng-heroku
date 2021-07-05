2d4fbcdb252c403d1bf47241be35bf695dc1b832
Danila Demidenko <cyxapuk.cyxapu75@gmail.com>
2021-07-05 14:14:05 +0300

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BottleComponent } from './bottle/bottle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BottleComponent
   ],
   imports: [
     BrowserModule
